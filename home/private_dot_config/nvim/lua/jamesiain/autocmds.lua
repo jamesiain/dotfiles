7d97cec70251ae85702c6b85876c7b92bdccb8b5
James MacMillan <jamesiain@me.com>
2023-01-23 12:28:30 -0500

    neovim: Convert start-at-prev-line autocmd to Lua

--- home/private_dot_config/nvim/lua/jamesiain/autocmds.lua
+++ home/private_dot_config/nvim/lua/jamesiain/autocmds.lua
@@ -1,17 +1,21 @@
 local api = vim.api
 local nvim_ex = api.nvim_create_augroup("NeoVimEx", { clear = true })
 
--- when editing a file, always jump to the last known cursor position
--- except when editing Git commit messages, when the position is invalid, or
--- when inside an event handler (which happens when a file is dropped into gvim)
+-- when editing a file, jump to the last known cursor position (in most cases)
 api.nvim_create_autocmd("BufReadPost", {
   group = nvim_ex,
   pattern = "*",
-  command = [[
-    if &ft != 'gitcommit' && line("'\"") > 1 && line("'\"") <= line("$")
-      execute "normal! g`\""
-    endif
-  ]],
+  callback = function()
+    if vim.bo.filetype == "gitcommit" then
+      return -- always start Git commits with cursor at line 0
+    end
+
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
 })
 
 local nvim_hl = api.nvim_create_augroup("NeoVimHighlight", { clear = true })
