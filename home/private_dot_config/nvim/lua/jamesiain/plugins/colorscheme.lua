973a542c0b3f93dfeab5f917221e0aa9137076da
James MacMillan <jamesiain@me.com>
2023-02-10 12:16:59 -0500

    neovim: Add colorscheme-persist and transparent.nvim

--- home/private_dot_config/nvim/lua/jamesiain/plugins/colorscheme.lua
+++ home/private_dot_config/nvim/lua/jamesiain/plugins/colorscheme.lua
@@ -1,4 +1,63 @@
 return {
+
+  -- toggle background transparency
+  {
+    "xiyaowong/nvim-transparent",
+    event = { "BufReadPre", "BufNewFile" },
+  },
+
+  -- persistent colorscheme picker
+  {
+    "jamesiain/colorscheme-persist.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    priority = 1000,
+    dependencies = {
+      "nvim-telescope/telescope-dap.nvim",
+      "xiyaowong/nvim-transparent",
+    },
+    keys = {
+      {
+        "<leader>cs",
+        function()
+          require("colorscheme-persist").picker()
+        end,
+        mode = { "n" },
+        desc = "Select Colorscheme",
+      },
+    },
+    opts = function()
+      local os_sep = require("plenary.path").path.sep
+      return {
+        file_path = vim.fn.stdpath "state" .. os_sep .. "colorscheme-persist.lua",
+        fallback = "habamax", -- least offensive built-in colorscheme
+        disable = { -- do not include built-in colorschemes in picker
+          "blue",
+          "darkblue",
+          "default",
+          "delek",
+          "desert",
+          "elflord",
+          "evening",
+          "industry",
+          "koehler",
+          "lunaperche",
+          "morning",
+          "murphy",
+          "pablo",
+          "peachpuff",
+          "quiet",
+          "ron",
+          "shine",
+          "slate",
+          "torte",
+          "zellner",
+        },
+        enable_preview = true,
+      }
+    end,
+  },
+
+  -- colorschemes
   { "shaunsingh/oxocarbon.nvim" },
   { "ellisonleao/gruvbox.nvim" },
   { "rose-pine/neovim", name = "rose-pine" },
@@ -9,7 +68,7 @@ return {
     opts = function()
       return {
         style = "moon",
-        transparent = true,
+        -- transparent = true,
         -- styles = {
         --   sidebars = "transparent",
         --   floats = "transparent",
